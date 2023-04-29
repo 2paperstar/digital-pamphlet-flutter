44e4e68b340d8d013efbae70cf9ed2c6a2c66eb7
2paperstar <lee@paperst.ar>
2023-04-29 15:36:44 +0900

    feat: notification item

--- lib/notification/presentation/view/notification_list.dart
+++ lib/notification/presentation/view/notification_list.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NotificationList extends StatelessWidget {
   const NotificationList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.separated(
+      itemCount: 40,
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      itemBuilder: (context, index) => ListTile(
+        title: const Text('Notification'),
+        subtitle: const Text('description'),
+        leading: const CircleAvatar(
+          child: Text('N'),
+        ),
+        trailing: Text(DateFormat.jm().format(DateTime.now())),
+      ),
+      separatorBuilder: (context, index) => const Divider(),
+    );
   }
 }
